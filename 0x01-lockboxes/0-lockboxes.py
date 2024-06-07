11d5d0ed62a827fad8cc71b092f658644890dcdf
Sahar Mohamed <100488384+SaharMFR@users.noreply.github.com>
2024-06-07 04:25:07 +0300

    Task-0 0x01

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Lockboxes """
+
+
+def canUnlockAll(boxes):
+    """
+    We have n number of locked boxes in front of you. Each box is numbered
+    sequentially from 0 to n - 1 and each box may contain keys to the other
+    boxes.
+    This function determines if all the boxes can be opened.
+
+    Parameters:
+    boxes: The list of boxes with some keys in each one (list of lists).
+
+    Return:
+    True if all boxes can be opened, False otherwise.
+    """
+    opened_boxes = [0]
+    n = len(boxes)
+    for opened_box in opened_boxes:
+        for key in boxes[opened_box]:
+            if key not in opened_boxes and 0 < key < n:
+                opened_boxes.append(key)
+
+    if len(opened_boxes) == n:
+        return True
+    return False
