a3574363466aba3dbb4ea6e2f4c6b76f11f6aa47
SaharMFR <sweetysahora@gmail.com>
2024-07-11 15:49:04 -0700

    Update in `semistandard` in task-0 0x06

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,13 +2,13 @@
 const request = require('util').promisify(require('request'));
 
 async function starWarsCharacters () {
-    let url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-    const response = await (await request(url)).body;
-    const chars = JSON.parse(response).characters;
-    for (let i = 0; i < chars.length; i++) {
-        let character = await (await request(chars[i])).body;
-        console.log(JSON.parse(character).name);
-    }
+  const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+  const response = await (await request(url)).body;
+  const chars = JSON.parse(response).characters;
+  for (let i = 0; i < chars.length; i++) {
+    let character = await (await request(chars[i])).body;
+    console.log(JSON.parse(character).name);
+  }
 }
 
 starWarsCharacters();
